4ea0bc1924b5499600bd82c92c8f74e935f03e2d
xdev1331 <131064070+xdev1331@users.noreply.github.com>
2023-04-23 13:58:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT current_user()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Fruit load list contains:")
 streamlit.text(my_data_row)
